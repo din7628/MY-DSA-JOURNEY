a430519f31d8a2f3c2db2a880b3aa0c0f435232a
M.H.D Bharadwaj <78069207+din7628@users.noreply.github.com>
2023-05-01 14:49:44 +0530

    Create COUNT_DIGITS.cpp

---
+++ A-Z SHEET PROBLEMS/BASIC MATHS/COUNT_DIGITS.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+//METHOD 1,TC=O(n),SC=O(1)
+int count_digits( int n )
+{
+   int x = n; int count =0;
+   while( x !=0 ) 
+   {
+       x = x / 10;
+       count++;
+   }
+   return count;
+}
+/* METHOD 2,TC=O(1),SC=O(1) USING to_string()
+
+int count_digits( int n )
+{
+  string x = to_string(n);
+  return x.length(); 
+}
+*/
+/* METHOD 3,TC=O(1),SC=O(1) using log function
+int count_digits( int n )
+{
+  int digits = floor(log10(n) + 1);
+  return digits;
+}
+*/
